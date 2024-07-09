6c9aa7009a69643f663385847a4c846391ff051a
JuanMa Garrido <juanma.garrido@gmail.com>
2024-07-09 18:12:40 +0100

    removed write_log function def

--- themes/block-style-examples/functions.php
+++ themes/block-style-examples/functions.php
@@ -10,15 +10,6 @@
  * @package     wp-dev-box
  * @since       1.0.0
  */
-function write_log( $data ) {
-	if ( true === WP_DEBUG ) {
-		if ( is_array( $data ) || is_object( $data ) ) {
-			error_log( print_r( $data, true ) );
-		} else {
-			error_log( $data );
-		}
-	}
-}
 
 // Setup.
 define( 'THEME_STYLE_URL', get_stylesheet_uri() );
